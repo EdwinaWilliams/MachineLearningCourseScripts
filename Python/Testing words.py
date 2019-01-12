82cba66d08f69898518932da1c02e466b75e90d2
edwinagw <egwilliams1991@gmail.com>
2019-01-12 23:35:54 +0200

    Playing around with the replace function in python

---
+++ Python/Testing words.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 23:12:21 2019
+
+@author: edwinaw
+"""
+
+
+word = "Edwina"
+
+a = word[4]
+
+print(a)
+
+new = word.replace(word[4],  "_")
+
+
+print(new)
