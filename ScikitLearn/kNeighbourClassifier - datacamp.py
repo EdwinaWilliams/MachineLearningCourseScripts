8197d7276920ab34f9657b8ee3ef42cb28b8ec34
edwinagw <egwilliams1991@gmail.com>
2019-02-07 19:56:35 +0200

    Complete code to classify and plot learning plot

--- ScikitLearn/kNeighbourClassifier - datacamp.py
+++ ScikitLearn/kNeighbourClassifier - datacamp.py
@@ -1,14 +1,19 @@
-
-# Import KNeighborsClassifier from sklearn.neighbors
+# Import necessary modules
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
-# Create arrays for the features and the response variable
-y = df['party'].values
-X = df.drop('party', axis=1).values
+# Create feature and target arrays
+X = digits.data
+y = digits.target
 
-# Create a k-NN classifier with 6 neighbors
-knn = KNeighborsClassifier(n_neighbors=6)
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
 
-# Fit the classifier to the data
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(7)
 
-knn.fit(X , y)
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test,y_test))
